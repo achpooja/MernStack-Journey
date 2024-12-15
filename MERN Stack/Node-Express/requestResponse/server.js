620c878eddc021af86f5ac5ad957bd84db64089d
achpooja <pa2328138@gmail.com>
2024-12-15 21:18:36 +0545

    Request and Responses

---
+++ MERN Stack/Node-Express/requestResponse/server.js
@@ -0,0 +1,70 @@
+//  Status Code //
+
+// 200 - OK 
+// 301 - Resource moved 
+// 404 - Not Found 
+// 500 - Internal server error
+// 100 Range - informational responses 
+// 200 Range - success codes 
+// 300 Range - codes for redirects 
+// 400 Range - used or client error codes
+// 500 Range - server error codes
+
+const http = require('http')
+const fs =require('fs')
+
+
+const server= http.createServer((req,res)=>{
+    console.log(req.url,req.method)
+
+// set header content type
+res.setHeader('Content-Type', 'text/html')
+// res.write('<head><link rel="stylesheet" href="#"></link></head>')
+// res.write('<p>Hello warrior!</p>')
+// res.write('<p>How you doing!</p>  ')
+
+// basic routing //
+
+let path ='./views/'
+
+switch(req.url){
+    case '/':
+        path+='index.html'
+        res.statusCode = 200
+        break
+    case '/about':
+        path+='about.html'
+        res.statusCode = 200
+        break
+
+        // redirects //
+    case '/about-me':
+        res.statusCode = 301
+        res.setHeader('Location', '/about')
+        res.end()
+        break    
+    default:
+        path+='404.html' 
+        res.statusCode = 404
+        break       
+
+}
+
+// send an html file //
+
+fs.readFile(path,(err,data)=>{
+      if(err){
+        console.log(err)
+        res.end()
+      } else{
+        res.write(data)
+        res.end()
+      }
+})
+// res.end()
+
+})
+
+server.listen(3000,'localhost',()=>{
+    console.log('listening on port 3000')
+})
