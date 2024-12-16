3b27f8e7879e59bd544d007f4c59e2d964a4cb36
achpooja <pa2328138@gmail.com>
2024-12-16 14:35:48 +0545

    Express setup made

---
+++ MERN Stack/Node-Express/expressApp.js
@@ -0,0 +1,30 @@
+const express =require('express');
+
+
+// express app //
+const app = express();
+
+// listen for requests//
+
+app.listen(3030)
+app.get('/',(req,res)=>{
+    res.sendFile('./requestResponse/views/index.html',{root: __dirname});
+// res.send('<p>Home Page</p>')
+})
+app.get('/about',(req,res)=>{
+    res.sendFile('./requestResponse/views/about.html',{root: __dirname});
+
+    // res.send('<p>Home Page</p>')
+    })
+
+
+// 404 Page //
+
+app.use((req, res)=>{
+    res.sendFile('./requestResponse/views/404.html',{root: __dirname})
+})
+// redirects //
+
+app.get('/about-me',(req,res)=>{
+    res.redirect('/about')
+})
